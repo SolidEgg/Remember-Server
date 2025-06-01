70715942e59f07bbb26f212aeff07ab7b318f792
suzinlim <122861956+suzinlim@users.noreply.github.com>
2025-06-01 19:45:06 +0900

    🎉 init: 프로젝트 세팅

---
+++ src/test/java/com/solidegg/remember/RememberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solidegg.remember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
